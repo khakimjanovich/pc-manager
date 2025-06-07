13376b22dc975cb9fc4a20cfdabfd94a5d616e31
Yunusali Abduraxmanov <37039084+khakimjanovich@users.noreply.github.com>
2025-06-07 14:53:46 +0500

    Update 2025_05_10_000002_create_pc_manager_card_add_sms_configs_table.php

--- database/migrations/2025_05_10_000002_create_pc_manager_card_add_sms_configs_table.php
+++ database/migrations/2025_05_10_000002_create_pc_manager_card_add_sms_configs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->string('language')->nullable()->index()->comment('Use ISO 639-2 codes for language like using 3 words instead of 2');
             $table->string('ussd')->nullable()->comment('USSD-kod orqali');
             $table->string('hash')->comment('autocomplete uchun hash');
-            $table->unsignedInteger('template_id')->comment("Sms templateni qo'yish uchun ishlatiladi, birinchi bo'lib templateni berish kerak keyin ular o'zlari tomondan ro'yxatdan o'tkazadi va bizga shuni idsini berishadi")->index();
+            $table->unsignedInteger('template_id')->nullable()->comment("Sms templateni qo'yish uchun ishlatiladi, birinchi bo'lib templateni berish kerak keyin ular o'zlari tomondan ro'yxatdan o'tkazadi va bizga shuni idsini berishadi")->index();
             $table->string('service_name')->comment('Sms qaysi servisdan kelyapganini aniqlashtirish uchun ishlatiladi');
             $table->string('processing_centre');
             $table->timestamps();
