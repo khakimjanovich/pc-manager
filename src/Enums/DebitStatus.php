b27832830ed6b99a75bdac80d8d74042290e3d67
Yunusali Abduraxmanov <37039084+khakimjanovich@users.noreply.github.com>
2025-06-14 01:43:38 +0500

    Update DebitStatus.php

--- src/Enums/DebitStatus.php
+++ src/Enums/DebitStatus.php
@@ -13,4 +13,17 @@ enum DebitStatus: string
     case CONFIRM_PC = 'confirm_pc';
     case ERROR_PC = 'error_pc';
     case UNKNOWN = 'unknown';
+    case DISMISS_PC = 'dismiss_pc';
+    case EXPIRED_PC = 'expired_pc';
+    case DISMISS_ERROR = 'dismiss_error';
+
+    public static function mapFromSV(int $status): DebitStatus
+    {
+        return match ($status) {
+            0 => self::INIT_PC,
+            1 => self::CONFIRM_PC,
+            2 => self::DISMISS_PC,
+            3 => self::EXPIRED_PC,
+        };
+    }
 }
