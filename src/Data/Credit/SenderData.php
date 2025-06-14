119835316ffb77084e628764d2a3011e48ed47e3
Yunusali Abduraxmanov <37039084+khakimjanovich@users.noreply.github.com>
2025-06-14 13:14:21 +0500

    Update SenderData.php

--- src/Data/Credit/SenderData.php
+++ src/Data/Credit/SenderData.php
@@ -9,17 +9,20 @@ use Spatie\LaravelData\Data;
 final class SenderData extends Data
 {
     public function __construct(
-        public string $retrieval_ref_number,
-        public string $nationality,
-        public string $doc_type,
-        public string $serial_no,
-        public string $id_card,
-        public string $doc_valid_through,
-        public string $person_code,
-        public string $surname,
-        public string $pan,
         public string $first_name,
+        public string $surname,
         public string $middle_name,
         public string $birth_date,
+        public string $doc_type,
+        public string $doc_expiry_date,
+        public string $doc_series_and_number,
+        public string $person_code,
+        public string $nationality,
+
+        public string $card_id_or_pan,
+        public string $debit_id,
+
+        public string $system,
+        public string $system_legal_name,
     ) {}
 }
