c1ae60d0bc9ace8dc36eacb5a992b76fe020d211
Yunusali Abduraxmanov <37039084+khakimjanovich@users.noreply.github.com>
2025-06-14 12:33:35 +0500

    Update PerformPayload.php

--- src/Data/Credit/PerformPayload.php
+++ src/Data/Credit/PerformPayload.php
@@ -9,17 +9,10 @@ use Spatie\LaravelData\Data;
 final class PerformPayload extends Data
 {
     public function __construct(
-        public string $retrieval_ref_number,
-        public string $nationality,
-        public SenderData $doc_type,
-        public string $serial_no,
-        public string $id_card,
-        public string $doc_valid_through,
-        public string $person_code,
-        public string $surname,
-        public string $pan,
-        public string $first_name,
-        public string $middle_name,
-        public string $birth_date,
+        public SenderData $sender,
+        public string $amount,
+        public string $recipient,
+        public string $recipient_type,
+        public ?string $phone_number
     ) {}
 }
