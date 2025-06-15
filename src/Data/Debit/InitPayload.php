4a7c1e52ae628bf28c73bb95974a09dcfdc56661
Yunusali Abduraxmanov <37039084+khakimjanovich@users.noreply.github.com>
2025-06-15 23:25:58 +0500

    Update Card.php

--- src/Data/Debit/InitPayload.php
+++ src/Data/Debit/InitPayload.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Khakimjanovich\PCManager\Data\Debit;
 
+use Khakimjanovich\PCManager\Models\Card;
 use Spatie\LaravelData\Data;
 
 final class InitPayload extends Data
 {
     public function __construct(
-        public string $card_id,
+        public Card $card,
         public string $amount,
     ) {}
 }
--- src/Data/P2P/PerformPayload.php
+++ src/Data/P2P/PerformPayload.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Khakimjanovich\PCManager\Data\P2P;
 
+use Khakimjanovich\PCManager\Models\Card;
 use Spatie\LaravelData\Data;
 
 final class PerformPayload extends Data
 {
     public function __construct(
-        public string $card_id,
+        public Card $card,
         public string $amount,
         public string $receiver,
         public ?string $fee_amount,
